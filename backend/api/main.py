faeac051c2411757b982a6846958435761b79442
hoanglegia <hoanghoanglelegia@gmail.com>
2025-04-03 20:46:59 +0700

    add api

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
-<<<<<<< HEAD
-from api.version_1 import auth, users, products, orders, cart, categories, sales, suppliers
-=======
-from api.version_1 import auth, users, products, orders, cart, categories, role
-from api.version_1 import auth, users, products, orders, cart, categories, orders_return, payments
->>>>>>> e7ca64d4a69776f7b0d09598de69d377079ba893
+from api.version_1 import auth, users, products, orders, cart, categories, orders_return, payments, sales, suppliers, role
 
 api_v1_router = APIRouter(
     prefix="/api/v1",
@@ -18,13 +13,10 @@ api_v1_router.include_router(products.router, tags=["products"], prefix="/produc
 api_v1_router.include_router(orders.router, tags=["orders"], prefix="/orders")
 api_v1_router.include_router(cart.router, tags=["cart"], prefix="/cart")
 api_v1_router.include_router(categories.router, tags=["categories"], prefix="/categories")
-<<<<<<< HEAD
 api_v1_router.include_router(sales.router, tags=["sales"], prefix="/sales")
 api_v1_router.include_router(suppliers.router, tags=["suppliers"], prefix="/suppliers")
-=======
-#api_v1_router.include_router(orders_return.router, tags=["orders_return"], prefix="/orders_return")
 api_v1_router.include_router(role.router, tags=["roles"], prefix="/roles")
 api_v1_router.include_router(orders_return.router, tags=["orders_return"], prefix="/orders_return")
 api_v1_router.include_router(payments.router, tags=["payments"], prefix="/payments")
 
->>>>>>> e7ca64d4a69776f7b0d09598de69d377079ba893
+
